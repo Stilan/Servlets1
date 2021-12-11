dde1e59cde1fab468dd8855188b75f47bae0b9f2
Stilan <40999153+Stilan@users.noreply.github.com>
2021-12-12 00:03:23 +0700

    Cookie

---
+++ src/main/java/ru/myServlet/servlet2/HomeServlet.java
@@ -0,0 +1,29 @@
+package ru.myServlet.servlet2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+          req.getServletContext().getRequestDispatcher("/home.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // получаем параметр запроса
+        String color = req.getParameter("color");
+        // создаем Cookie с данным значением
+        Cookie colorCookie = new Cookie("color", color);
+        // кладем в ответ
+        resp.addCookie(colorCookie);
+        // перенаправляем пользователя обратно на страницу home
+        resp.sendRedirect(req.getContextPath() + "/home");
+    }
+}
